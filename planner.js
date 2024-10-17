a17f382a9c7baff38a5b5dfdee13a6f0477ffb49
rmcarvajal <darwyn089@gmail.com>
2024-10-17 08:25:02 -0500

    fix input id, delete item from array

--- planner.js
+++ planner.js
@@ -2,7 +2,15 @@ const textbox = document.getElementById("actbox")
 
 const pendBox = document.getElementById("pend")
 
+const ongBox = document.getElementById("ong")
+
+const endBox = document.getElementById("end")
+
 const pendList=[]
+
+const ongList=[]
+
+const endList=[]
 
 function addTask(){
     const taskInput = document.getElementById("input")
@@ -16,10 +24,38 @@ function addTask(){
                 <div class="pendItem">
                     <h2>${value}</h2>
                     <div class="pendButton">
-                        <button>🠊</button>
+                        <button onclick="pendToOng()">🠊</button>
                     </div>
                 </div>
         `
+        console.log(pendList)
     }
+}
+
+function pendToOng(){
+    const taskInput = pendList
+    const value = taskInput.value
+    ongList.push=[value]
+    ongBox.innerHTML+=`
+                <div class="ongItem">
+                    <h2>${value}</h2>
+                    <div class="ongButton">
+                        <button onclick="addTask()">🠈</button>
+                        <button onclick="ongToEnd()">🠊</button>
+                    </div>
+                </div>
+        `
+    
+}
+
+function ongToEnd(){
+    const taskInput = document.getElementById("input")
+    const value = taskInput.value
+    endList.push=[value]
+    endBox.innerHTML+=`
+                <div class="endItem">
+                    <h2>${value}</h2>
+                </div>
+        `
 }
 
